0a5bdad5fe24fdb54d9aeefc854e826a06838015
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-02-14 09:45:01 +0100

    Initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+
+export class Tab1Page {
+
+  // Dit is een javascript-object
+  private cardContent = { subtitle: 'Donald Duck',
+                          title   : 'Zwarte Magica',
+                          image   : './assets/images/zwarte-magica.png',
+                          content : 'Zij wil altijd het geluksdubbeltje van Dagobert Duck stelen. Dit is niet correct...'};
+
+  private cardContent01 = { subtitle: 'Donald Duck',
+                          title   : 'Zware Jongens',
+                          image   : './assets/images/zware-jongens.gif',
+                          content : 'Zij willen altijd de kluis van Dagobert Duck stelen.'};
+
+
+}
